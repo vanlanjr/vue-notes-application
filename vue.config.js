94d765e6cdd96d8d4313bfc1a221448af698a58e
vanlanjr <vanlanjr@dukes.jmu.edu>
2021-04-30 17:50:55 -0400

    Added deployment code

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
+// module.exports = {
+//   transpileDependencies: [
+//     'vuetify'
+//   ]
+// }
+
 module.exports = {
-  transpileDependencies: [
-    'vuetify'
-  ]
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vue-notes-application/'
+    : '/'
 }
